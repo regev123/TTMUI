812b46d487cfbb10721a1397b18c03016b6fce78
Regev Avital <45341050+regev123@users.noreply.github.com>
2025-09-02 08:41:18 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-// Import required modules
-const express = require('express'); // Express framework for building the API
-const config = require('config'); // Configuration module to access environment settings
-const path = require('path'); // Module for working with file and directory paths
-const app = express(); // Initialize the Express application
-const { exec } = require('child_process'); // Module to run shell commands
+const express = require('express');
+const config = require('config');
+const path = require('path');
+const http = require('http');
+const WebSocket = require('ws');
+const clients = require('./utils/websocketClients');
+const { exec } = require('child_process');
 
-// Get hostname and port from the config file
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  clients.add(ws);
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+
+  ws.on('error', (error) => console.error('WebSocket error:', error));
+});
+
 const hostname = config.get('hostname');
 const port = config.get('port');
 
-// Serve static files from the React app's 'build' directory
-// This is where the production-ready React frontend is served from
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'client/build/')));
+app.use(express.json());
 
-// Initialize middleware for parsing JSON request bodies
-// extended: false means it uses the classic body-parser library
-app.use(express.json({ extended: false }));
-
-// Register API routes in a loop for cleaner and more maintainable code
-
-app.use('/api/packager', require('./routes/api/packager'));
-app.use('/api/deployer', require('./routes/api/deployer'));
+// Register API routes
+app.use('/api/packagerDeployer', require('./routes/api/packagerDeployer'));
 app.use('/api/configuration', require('./routes/api/configuration'));
 app.use('/api/installation', require('./routes/api/installation'));
 app.use('/api/util', require('./routes/api/util'));
@@ -30,18 +41,16 @@ app.use(
   require('./routes/api/environmentValidation')
 );
 app.use('/api/validation', require('./routes/api/validation'));
+app.use('/api/obfuscation', require('./routes/api/obfuscation'));
 
-// Catch-all handler to serve the React app's index.html for any route not explicitly defined above
-// This enables the React app to handle frontend routing via its client-side router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-// Start the server and listen on the specified hostname and port
-app.listen(port, hostname, () => {
-  console.log(`Server started on port ${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server and WebSocket listening on port ${port}`);
 
   // for Production: Open the app in the default browser after the server starts
-  //const url = `http://localhost:${port}/`;
-  //exec(`start ${url}`);
+  // const url = `http://localhost:${port}/`;
+  // exec(`start ${url}`);
 });
