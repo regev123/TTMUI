57093c93d381a36b243f83ff00a8ec3956cb485d
Regev Avital <45341050+regev123@users.noreply.github.com>
2025-09-02 08:46:34 +0300

    Add files via upload

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
-// Create an instance of axios
+/**
+ * @module api
+ * @desc    Configures an axios instance with a base URL and common headers to be used for API requests.
+ *          This setup ensures consistent API calls with the proper base URL and headers for JSON content.
+ *
+ * Axios Instance Configuration:
+ * - `baseURL`: The base URL for all requests, in this case set to `/api`.
+ * - `headers`: The default headers to include in each request, setting `Content-Type` to `application/json` for RESTful API communication.
+ *
+ * @returns {AxiosInstance} - The configured axios instance to be used for making HTTP requests.
+ */
 const api = axios.create({
   baseURL: '/api',
   headers: {
