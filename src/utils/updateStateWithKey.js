3ed51ca96fa34d07651748985089e25d7dd0b63e
Regev Avital <45341050+regev123@users.noreply.github.com>
2025-09-03 09:15:24 +0300

    Delete src directory

--- src/utils/updateStateWithKey.js
+++
@@ -1,8 +0,0 @@
-const updateStateWithKey = async (setStateFunction, key, value) => {
-  setStateFunction((prevState) => ({
-    ...prevState,
-    [key]: value,
-  }));
-};
-
-export default updateStateWithKey;
